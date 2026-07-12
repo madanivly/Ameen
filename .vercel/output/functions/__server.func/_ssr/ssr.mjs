405a1153dad1c09e89a8de551d85dbdf996e70f2
admin <admin@Mac-mini.local>
2026-07-12 21:56:47 +0300

    Clean build and redeploy

--- .vercel/output/functions/__server.func/_ssr/ssr.mjs
+++ .vercel/output/functions/__server.func/_ssr/ssr.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-D7htdZL-.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-BNN1GYn6.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
